8b88c562a4dc57d19a6b7841bd20a48f7f24e8eb
tulabulaam <jastrzebski@o2.pl>
2026-09-24 17:20:08 +0200

    Corrections

--- plugin.py
+++ plugin.py
@@ -119,6 +119,9 @@ class BasePlugin:
             Devices[8].Update(0,str(Import_power))
             Devices[9].Update(0,str(Export_power))
             Devices[10].Update(0,str(Export_power)+";"+str(Export_Wh_since_last_reset*1000))
+            Devices[11].Update(0,str(L1_voltage))
+            Devices[12].Update(0,str(L2_voltage))
+            Devices[13].Update(0,str(L3_voltage))
             
             
             if Parameters["Mode6"] == 'Debug':
